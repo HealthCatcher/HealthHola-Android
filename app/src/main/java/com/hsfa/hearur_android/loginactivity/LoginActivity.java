6f1370c61f8c540c04e426f4f1c700a35cea76b2
byun0-0 <isly7959@naver.com>
2024-07-02 09:16:56 +0900

    feat: login 형식 변경

--- app/src/main/java/com/hsfa/hearur_android/loginactivity/LoginActivity.java
+++ app/src/main/java/com/hsfa/hearur_android/loginactivity/LoginActivity.java
@@ -18,9 +18,6 @@ import com.hsfa.hearur_android.databinding.ActivityLoginBinding;
 import com.hsfa.hearur_android.dto.UserInfo;
 import com.hsfa.hearur_android.network.FetchJWTApiService;
 import com.hsfa.hearur_android.network.FetchJWTResponse;
-import com.hsfa.hearur_android.network.NaverApiService;
-import com.hsfa.hearur_android.network.NaverProfileRepository;
-import com.hsfa.hearur_android.network.NaverProfileResponse;
 import com.navercorp.nid.NaverIdLoginSDK;
 
 import android.content.Context;
@@ -33,12 +30,9 @@ import com.navercorp.nid.profile.data.NidProfileMap;
 import com.navercorp.nid.profile.data.NidProfileResponse;
 
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.disposables.Disposable;
 import okhttp3.Headers;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -48,9 +42,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private NaverProfileRepository naverProfileRepository;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-
     private static final String TAG = "LoginActivity";
 
     private ActivityLoginBinding binding;
@@ -59,66 +51,68 @@ public class LoginActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
 
-    private String clientId = String.valueOf(R.string.client_id);
-    private String clientSecret = String.valueOf(R.string.client_secret);
-    private String clientName = String.valueOf(R.string.client_name);
+    private String clientId;
+    private String clientSecret;
+    private String clientName;
+
+
     // 백엔드에 jwt토큰 요청
-    private Observable<String> fetchJWT(UserInfo userInfo) {
-        return Observable.create(emitter -> {
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.219.100:8080/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+    private void fetchJWT(UserInfo userInfo) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.219.100:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-            FetchJWTApiService fetchJWTApiService = retrofit.create(FetchJWTApiService.class);
-            Call<FetchJWTResponse> call = fetchJWTApiService.getJWT(userInfo);
-            call.enqueue(new Callback<FetchJWTResponse>() {
-                @Override
-                public void onResponse(@NonNull Call<FetchJWTResponse> call, @NonNull Response<FetchJWTResponse> response) {
-                    if (response.isSuccessful() && response.body() != null) {
-                        Headers headers = response.headers();
-                        String jwt = headers.get("Authorization");
-                        if (jwt != null) {
-                            emitter.onNext(jwt);
-                            emitter.onComplete();
-                        } else {
-                            emitter.onError(new Throwable("JWT is null"));
-                        }
+        FetchJWTApiService fetchJWTApiService = retrofit.create(FetchJWTApiService.class);
+        Call<FetchJWTResponse> call = fetchJWTApiService.getJWT(userInfo);
+        call.enqueue(new Callback<FetchJWTResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<FetchJWTResponse> call, @NonNull Response<FetchJWTResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Headers headers = response.headers();
+                    String jwt = (String) headers.get("Authorization");
+                    if (jwt != null) {
+                        sharedPreferences = getSharedPreferences("my_app_pref", MODE_PRIVATE);
+                        editor = sharedPreferences.edit();
+                        String saveJWT = jwt.split(" ")[1];
+                        editor.putString("jwt", saveJWT);
+                        editor.apply();
+                        Log.d(TAG, "onResponse: JWT token: " + jwt);
                     } else {
-                        emitter.onError(new Throwable("Request failed: " + response.message()));
-                        Log.e(TAG, "onResponse: "+ response.message());
+                        Log.e(TAG, "onResponse: JWT token is null");
                     }
+                } else {
+                    Log.e(TAG, "onResponse: " + response.message());
                 }
+            }
 
-                @Override
-                public void onFailure(@NonNull Call<FetchJWTResponse> call, @NonNull Throwable t) {
-                    Log.e(TAG, "onFailure: ", t);
-                    emitter.onError(t);
-                }
-            });
+            @Override
+            public void onFailure(@NonNull Call<FetchJWTResponse> call, @NonNull Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
         });
     }
-    // 네이버 프로필 정보 가져오기
-    private void getUserProfile(String accessToken) {
-        Disposable disposable = naverProfileRepository.requestNaverUserProfile(accessToken)
-                .flatMap(this::fetchJWT)
-                .subscribe(
-                        jwtToken -> {
-                            // JWT 토큰을 사용하여 필요한 작업 수행
-                            Log.d("NaverProfile", "JWT Token: " + jwtToken);
-                            sharedPreferences = getSharedPreferences("my_app_pref", MODE_PRIVATE);
-                            editor = sharedPreferences.edit();
-                            editor.putString("jwt", jwtToken);
-                            editor.apply();
-                        },
-                        throwable -> {
-                            // 오류 처리
-                            Log.e("NaverProfile", "Failed to fetch JWT token: " + throwable.getMessage());
-                        }
-                );
-
-        compositeDisposable.add(disposable); // Disposable을 compositeDisposable에 추가
-    }
+// 네이버 프로필 정보 가져오기
+//    private void getUserProfile(String accessToken) {
+//        Disposable disposable = naverProfileRepository.requestNaverUserProfile(accessToken)
+//                .flatMap(this::fetchJWT)
+//                .subscribe(
+//                        jwtToken -> {
+//                            // JWT 토큰을 사용하여 필요한 작업 수행
+//                            Log.d("NaverProfile", "JWT Token: " + jwtToken);
+//                            sharedPreferences = getSharedPreferences("my_app_pref", MODE_PRIVATE);
+//                            editor = sharedPreferences.edit();
+//                            editor.putString("jwt", jwtToken);
+//                            editor.apply();
+//                        },
+//                        throwable -> {
+//                            // 오류 처리
+//                            Log.e("NaverProfile", "Failed to fetch JWT token: " + throwable.getMessage());
+//                        }
+//                );
+//
+//        compositeDisposable.add(disposable); // Disposable을 compositeDisposable에 추가
+//    }
 
     // 네이버 로그인 런처
     private final ActivityResultLauncher<Intent> launcher = registerForActivityResult(
@@ -128,7 +122,9 @@ public class LoginActivity extends AppCompatActivity {
                     // 성공
                     updateView();
                     String accessToken = NaverIdLoginSDK.INSTANCE.getAccessToken();
-                    getUserProfile(accessToken);
+                    UserInfo userInfo = new UserInfo("naver", accessToken);
+                    fetchJWT(userInfo);
+
                 } else if (result.getResultCode() == RESULT_CANCELED) {
                     // 실패 or 에러
                     String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
@@ -143,7 +139,9 @@ public class LoginActivity extends AppCompatActivity {
         // View Binding
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        naverProfileRepository = new NaverProfileRepository();
+        clientId = getResources().getString(R.string.client_id);
+        clientSecret = getResources().getString(R.string.client_secret);
+        clientName = getResources().getString(R.string.client_name);
         init();
     }
 
@@ -155,24 +153,24 @@ public class LoginActivity extends AppCompatActivity {
         NaverIdLoginSDK.INSTANCE.setShowMarketLink(true);
         NaverIdLoginSDK.INSTANCE.setShowBottomTab(true);
 
-        binding.buttonOAuthLoginImg.setOAuthLogin(new OAuthLoginCallback() {
-            @Override
-            public void onSuccess() {
-                updateView();
-            }
-
-            @Override
-            public void onFailure(int httpStatus, @NonNull String message) {
-                String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
-                String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
-                Toast.makeText(context, "errorCode:" + errorCode + ", errorDesc:" + errorDescription, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(int errorCode, @NonNull String message) {
-                onFailure(errorCode, message);
-            }
-        });
+//        binding.buttonOAuthLoginImg.setOAuthLogin(new OAuthLoginCallback() {
+//            @Override
+//            public void onSuccess() {
+//                updateView();
+//            }
+//
+//            @Override
+//            public void onFailure(int httpStatus, @NonNull String message) {
+//                String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
+//                String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
+//                Toast.makeText(context, "errorCode:" + errorCode + ", errorDesc:" + errorDescription, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onError(int errorCode, @NonNull String message) {
+//                onFailure(errorCode, message);
+//            }
+//        });
 
         // 로그인 Launcher
         binding.loginLauncher.setOnClickListener(new View.OnClickListener() {
@@ -184,30 +182,30 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // 로그인 Callback
-        binding.loginCallback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NaverIdLoginSDK.INSTANCE.setBehavior(NidOAuthBehavior.DEFAULT);
-                NaverIdLoginSDK.INSTANCE.authenticate(context, new OAuthLoginCallback() {
-                    @Override
-                    public void onSuccess() {
-                        updateView();
-                    }
-
-                    @Override
-                    public void onFailure(int httpStatus, String message) {
-                        String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
-                        String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
-                        Toast.makeText(context, "errorCode:" + errorCode + ", errorDesc:" + errorDescription, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(int errorCode, String message) {
-                        onFailure(errorCode, message);
-                    }
-                });
-            }
-        });
+//        binding.loginCallback.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                NaverIdLoginSDK.INSTANCE.setBehavior(NidOAuthBehavior.DEFAULT);
+//                NaverIdLoginSDK.INSTANCE.authenticate(context, new OAuthLoginCallback() {
+//                    @Override
+//                    public void onSuccess() {
+//                        updateView();
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int httpStatus, String message) {
+//                        String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
+//                        String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
+//                        Toast.makeText(context, "errorCode:" + errorCode + ", errorDesc:" + errorDescription, Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onError(int errorCode, String message) {
+//                        onFailure(errorCode, message);
+//                    }
+//                });
+//            }
+//        });
 
         // 로그아웃
         binding.logout.setOnClickListener(new View.OnClickListener() {
@@ -218,7 +216,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+        // 연동 끊기
         binding.deleteToken.setOnClickListener(v -> {
             new NidOAuthLogin().callDeleteTokenApi(new OAuthLoginCallback() {
                 @Override
--- app/src/main/java/com/hsfa/hearur_android/network/NaverApiService.java
+++ app/src/main/java/com/hsfa/hearur_android/network/NaverApiService.java
@@ -2,12 +2,13 @@ package com.hsfa.hearur_android.network;
 
 
 import io.reactivex.rxjava3.core.Observable;
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 
 public interface NaverApiService {
     @GET("v1/nid/me")
-    Observable<NaverProfileResponse> getNaverUserProfile(
+    Call<NaverProfileResponse> getNaverUserProfile(
             @Header("Authorization") String accessToken
     );
 
