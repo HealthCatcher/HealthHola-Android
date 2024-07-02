c3aaf18821d11615791b91b83fb94a24d73517af
byun0-0 <isly7959@naver.com>
2024-07-02 09:17:32 +0900

    feat: 체험단 탭 구성

--- app/src/main/java/com/hsfa/hearur_android/mainactivity/ui/experience/ExperienceFragment.java
+++ app/src/main/java/com/hsfa/hearur_android/mainactivity/ui/experience/ExperienceFragment.java
@@ -24,8 +24,8 @@ public class ExperienceFragment extends Fragment {
         binding = FragmentExperienceBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textExperience;
-        experienceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textExperience;
+        //experienceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
