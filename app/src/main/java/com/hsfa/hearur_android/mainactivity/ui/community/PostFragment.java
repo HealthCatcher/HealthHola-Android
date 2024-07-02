c458c96ac4043077e3ce9213833c7757e967ac5c
byun0-0 <isly7959@naver.com>
2024-07-02 09:16:27 +0900

    feat: 커뮤니티 게시글 작성 탭 구현

---
+++ app/src/main/java/com/hsfa/hearur_android/mainactivity/ui/community/PostFragment.java
@@ -0,0 +1,39 @@
+package com.hsfa.hearur_android.mainactivity.ui.community;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.hsfa.hearur_android.databinding.FragmentCommunityBinding;
+import com.hsfa.hearur_android.databinding.FragmentCommunityPostBinding;
+
+public class PostFragment extends Fragment {
+private FragmentCommunityPostBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        PostViewModel postViewModel =
+                new ViewModelProvider(this).get(PostViewModel.class);
+
+        binding = FragmentCommunityPostBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.textPost;
+        //postViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/hsfa/hearur_android/mainactivity/ui/community/PostViewModel.java
@@ -0,0 +1,17 @@
+package com.hsfa.hearur_android.mainactivity.ui.community;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PostViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public PostViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is post fragment");
+    }
+
+    public MutableLiveData<String> getText() {
+        return mText;
+    }
+}
