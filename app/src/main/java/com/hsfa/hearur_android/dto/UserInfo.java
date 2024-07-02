305e0e37686ab393498b9750368be56b01235547
byun0-0 <isly7959@naver.com>
2024-07-02 09:16:06 +0900

    feat: jwt요청 json형식 변경

--- app/src/main/java/com/hsfa/hearur_android/dto/UserInfo.java
+++ app/src/main/java/com/hsfa/hearur_android/dto/UserInfo.java
@@ -2,39 +2,13 @@ package com.hsfa.hearur_android.dto;
 
 public class UserInfo {
     private String provider;
-    private String providerId;
-    private String email;
-    private String name;
+    private String accessToken;
 
     public UserInfo() {
     }
 
-    public UserInfo(String provider, String providerId, String email, String name) {
+    public UserInfo(String provider, String accessToken) {
         this.provider = provider;
-        this.providerId = providerId;
-        this.email = email;
-        this.name = name;
-    }
-
-    public void setUserInfo(String provider, String providerId, String email, String name) {
-        this.provider = provider;
-        this.providerId = providerId;
-        this.email = email;
-        this.name = name;
-    }
-    public String getProvider() {
-        return provider;
-    }
-
-    public String getProviderId() {
-        return providerId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
+        this.accessToken = accessToken;
     }
 }
