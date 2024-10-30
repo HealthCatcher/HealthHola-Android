8923362e4678a39665dab426563d328302c13f6b
byun0-0 <isly7959@naver.com>
2024-10-30 23:25:53 +0900

    feat: setting activity 추가

---
+++ app/src/main/java/com/hsfa/hearur_android/activity/settingsactivity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.hsfa.hearur_android.activity.settingsactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hsfa.hearur_android.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
