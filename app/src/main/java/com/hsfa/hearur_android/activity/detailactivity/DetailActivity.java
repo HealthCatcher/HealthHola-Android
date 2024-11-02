b5e40686cbca67e9a6431e9ee4796f9ec558f277
byun0-0 <isly7959@naver.com>
2024-11-02 13:51:27 +0900

    fix: 상세페이지가 신청페이지로 넘어가는 현상 수정

--- app/src/main/java/com/hsfa/hearur_android/activity/detailactivity/DetailActivity.java
+++ app/src/main/java/com/hsfa/hearur_android/activity/detailactivity/DetailActivity.java
@@ -6,7 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hsfa.hearur_android.R;
-import com.hsfa.hearur_android.activity.detailactivity.fragment.ApplyFragment;
+import com.hsfa.hearur_android.activity.detailactivity.fragment.DetailFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class DetailActivity extends AppCompatActivity {
         // 기본 프래그먼트 로드 (예: 신청하기 화면)
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_detail_container, new ApplyFragment()) // 처음에 신청하기 화면 표시
+                    .replace(R.id.fragment_detail_container, new DetailFragment()) // 처음에 신청하기 화면 표시
                     .commit();
         }
     }
