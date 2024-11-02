3ab597c6594a86552fe56e7ec1accc97c2a51ce5
byun0-0 <isly7959@naver.com>
2024-11-02 13:42:53 +0900

    feat: DetailActivity 추가

---
+++ app/src/main/java/com/hsfa/hearur_android/activity/detailactivity/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.hsfa.hearur_android.activity.detailactivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hsfa.hearur_android.R;
+import com.hsfa.hearur_android.activity.detailactivity.fragment.ApplyFragment;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // 기본 프래그먼트 로드 (예: 신청하기 화면)
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_detail_container, new ApplyFragment()) // 처음에 신청하기 화면 표시
+                    .commit();
+        }
+    }
+}
