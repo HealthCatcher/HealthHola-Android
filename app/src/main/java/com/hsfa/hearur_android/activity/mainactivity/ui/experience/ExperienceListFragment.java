80836d0b3c939db5c6162ccf89e27c9662e2a04d
byun0-0 <isly7959@naver.com>
2024-11-04 00:54:22 +0900

    feat: 체험단 페이지 디테일 수정

--- app/src/main/java/com/hsfa/hearur_android/activity/mainactivity/ui/experience/ExperienceListFragment.java
+++ app/src/main/java/com/hsfa/hearur_android/activity/mainactivity/ui/experience/ExperienceListFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -42,9 +43,21 @@ public class ExperienceListFragment extends Fragment {
 
             TextView titleText = itemView.findViewById(R.id.card_title);
             TextView subtitleText = itemView.findViewById(R.id.card_subtitle);
+            TextView periodText = itemView.findViewById(R.id.card_period);
+            ImageView imageView = itemView.findViewById(R.id.card_product);
+            ImageView imagebanner = itemView.findViewById(R.id.card_image);
 
             titleText.setText(item.get("title"));
             subtitleText.setText(item.get("subtitle"));
+            periodText.setText("~" + item.get("period"));
+            if(item.containsKey("image")) {
+                int imageId = getResources().getIdentifier(item.get("image"), "drawable", getActivity().getPackageName());
+                imageView.setImageResource(imageId);
+            }
+            if(item.containsKey("banner")) {
+                int imageId = getResources().getIdentifier(item.get("banner"), "drawable", getActivity().getPackageName());
+                imagebanner.setImageResource(imageId);
+            }
 
             itemView.setOnClickListener(v -> {
                 Intent intent = new Intent(getActivity(), DetailActivity.class);
@@ -62,30 +75,38 @@ public class ExperienceListFragment extends Fragment {
 
         Map<String, String> item1 = new HashMap<>();
         item1.put("title", "비타민 C 보충제");
-        item1.put("subtitle", "면역력 강화 및 피로 회복에 도움");
+        item1.put("subtitle", "면역력 강화 및 피로 회복");
+        item1.put("period", "2024-11-05");
+        item1.put("image", "vitaminc");
         data.add(item1);
 
         Map<String, String> item2 = new HashMap<>();
         item2.put("title", "오메가-3 지방산");
         item2.put("subtitle", "심혈관 건강과 뇌 기능 개선");
+        item2.put("period", "2024-11-10");
+        item2.put("image", "omega3");
         data.add(item2);
 
         Map<String, String> item3 = new HashMap<>();
         item3.put("title", "프로바이오틱스");
         item3.put("subtitle", "장 건강 및 소화 개선에 도움");
+        item3.put("period", "2024-11-15");
         data.add(item3);
 
         Map<String, String> item4 = new HashMap<>();
         item4.put("title", "칼슘 보충제");
         item4.put("subtitle", "뼈 건강 및 골다공증 예방에 필수");
+        item4.put("period", "2024-11-20");
         data.add(item4);
 
         Map<String, String> item5 = new HashMap<>();
         item5.put("title", "단백질 파우더");
         item5.put("subtitle", "운동 후 근육 회복에 효과적");
+        item5.put("period", "2024-11-25");
         data.add(item5);
 
         return data;
     }
+
 
 }
